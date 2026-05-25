5a1512b4a1c0f7f8ab46abd5543e1221aa0624fe
Rafael Mechi <rafaelricardomechi@outlook.com>
2026-05-25 20:41:58 -0300

    Exercicio 073 feito
    Nao esta corrigido ainda, porem funciona normalmente.

--- mundo3/ex073.py
+++ mundo3/ex073.py
@@ -1,1 +1,7 @@
-    
+tabela = ('Palmeiras','Flamengo','Fluminense','Athletico-PR','Bragantino','Bahia','Sao Paulo','Coritiba','Cruzeiro','Botafogo','EC Vitoria','Atletico-MG','Internacional','Gremio','Corinthians','Vasco da Gama','Santos','Mirassol','Remo','Chapecoense')
+
+print(f'Lista de times do brasileirao: {tabela}')
+print(f'Os 5 primeiros sao: {tabela[0:5]}')
+print(f'Os 4 ultimos sao: {tabela[16:20]}')
+print(f'Times em ordem alfabetica: {tuple(sorted(tabela))}')
+print(f'A chapecoense esta na {tabela.index('Chapecoense')+1} posicao!')
