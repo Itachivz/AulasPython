586f1e6520978a3db2ae74191e73fa5e34692b51
Rafael Mechi <rafaelricardomechi@outlook.com>
2026-05-25 20:17:15 -0300

    Create aula16.py
    Criei o aula16.py
    Tem um pouco do que foi passado na aula 16

---
+++ mundo3/aula16.py
@@ -0,0 +1,9 @@
+lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
+
+for cont in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[cont]}!')
+
+#for pos, comida in enumerate(lanche):
+    #print(f'Eu vou comer {comida}, na posicao {pos}!')
+
+print('Eu comi muito!')
