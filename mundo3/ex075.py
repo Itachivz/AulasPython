5ec592df77f4fe4c2b9edff109047cac1e5d3b20
Rafael Mechi <rafaelricardomechi@outlook.com>
2026-05-25 21:14:51 -0300

    ex075.py Resolvido
    O exercicio esta resolvido e corrigido.

---
+++ mundo3/ex075.py
@@ -0,0 +1,20 @@
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite um numero: '))
+n3 = int(input('Digite um numero: '))
+n4 = int(input('Digite um numero: '))
+y = 0
+x = (n1, n2, n3, n4)
+
+if 9 in x:
+    y += 1
+
+print(f'Voce digitou os valores: {x}!')
+print(f'O valor 9 apareceu {y} vezes!')
+if 3 in x:
+    print(f'O valor 3 apareceu na posicao {x.index(3)+1}!')
+else:
+    print(f'O valor 3 nao foi digitado!')
+print(f'Os valores pares digitados foram: ', end='') # Abaixo foi feito pelo Prof. / Acima foi eu que fiz
+for n in n1, n2, n3, n4:
+    if n % 2 == 0:
+        print(n, end=', ')
